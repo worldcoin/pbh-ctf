36ef848e3694581b9e44539044ff8a59584c746e
0xOsiris <djosiris@proton.me>
2025-02-24 11:06:34 -0700

    feat: basic ctf bot

---
+++ crates/pbh-ctf/src/config.rs
@@ -0,0 +1,46 @@
+use std::path::Path;
+
+use reqwest::Url;
+use serde::Deserialize;
+
+pub const CONFIG_PREFIX: &str = "CTF_BOT";
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct CtfConfig {
+    /// Ethereum Signer
+    pub private_key: String,
+    /// The Wallet Nonce
+    pub wallet_nonce: u64,
+    /// The PBH nonce limit
+    pub pbh_nonce_limit: u8,
+    /// Semaphore secret
+    pub secret: String,
+    /// WC Sepolia Provider
+    pub provider: Url,
+    /// The start timestamp of the game
+    pub start_timestamp: u64,
+    /// The end timestamp of the game
+    pub end_timestamp: u64,
+}
+
+impl CtfConfig {
+    pub fn load(config_path: Option<&Path>) -> eyre::Result<Self> {
+        let mut settings = config::Config::builder();
+
+        if let Some(path) = config_path {
+            settings = settings.add_source(config::File::from(path).required(true));
+        }
+
+        let settings = settings
+            .add_source(
+                config::Environment::with_prefix(CONFIG_PREFIX)
+                    .separator("__")
+                    .try_parsing(true),
+            )
+            .build()?;
+
+        let config = settings.try_deserialize::<Self>()?;
+
+        Ok(config)
+    }
+}
