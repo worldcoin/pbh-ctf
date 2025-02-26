450bdad102b0c32a5aa62e177dd1ef010c9a3afd
0xKitsune <0xkitsune@protonmail.com>
2025-02-26 07:28:45 -0700

    add CTFTransactionBuilder

--- crates/pbh-helpers/src/helpers.rs
+++ crates/pbh-helpers/src/helpers.rs
@@ -1,6 +1,8 @@
-use alloy_consensus::TxEnvelope;
+use std::ops::{Deref, DerefMut};
+
+use alloy_consensus::{TxEnvelope, TypedTransaction};
 use alloy_network::{EthereumWallet, TransactionBuilder};
-use alloy_primitives::TxKind;
+use alloy_primitives::{Address, Bytes, TxKind};
 use alloy_rpc_types_eth::{TransactionInput, TransactionRequest};
 use alloy_signer_local::PrivateKeySigner;
 use alloy_sol_types::{SolCall, SolValue};
@@ -16,7 +18,10 @@ use world_chain_builder_pbh::{
 };
 use world_chain_builder_test_utils::{
     WC_SEPOLIA_CHAIN_ID,
-    bindings::{IMulticall3, IPBHEntryPoint},
+    bindings::{
+        IMulticall3::{self, Call3},
+        IPBHEntryPoint,
+    },
 };
 
 use crate::{INCLUSION_PROOF_URL, PBH_ENTRY_POINT};
@@ -26,6 +31,111 @@ use crate::{PBH_CTF_CONTRACT, PBHProof};
 pub struct InclusionProof {
     pub root: Field,
     pub proof: semaphore_rs::poseidon_tree::Proof,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct CTFTransactionBuilder(pub TransactionRequest);
+
+impl Deref for CTFTransactionBuilder {
+    type Target = TransactionRequest;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for CTFTransactionBuilder {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl CTFTransactionBuilder {
+    pub fn new() -> Self {
+        let tx = TransactionRequest::default()
+            .gas_limit(130000)
+            .max_fee_per_gas(1e8 as u128)
+            .max_priority_fee_per_gas(1e8 as u128)
+            .with_chain_id(WC_SEPOLIA_CHAIN_ID);
+
+        CTFTransactionBuilder(tx)
+    }
+
+    pub async fn with_pbh_multicall(
+        self,
+        identity: &Identity,
+        pbh_nonce: u16,
+        calls: Vec<Call3>,
+    ) -> Result<Self> {
+        // Get the inclusion proof for the identity in the from the World Tree
+        let proof = fetch_inclusion_proof(&identity).await?;
+
+        // Create the external nullifier hash
+        let date = chrono::Utc::now().naive_utc().date();
+        let date_marker = DateMarker::from(date);
+        let external_nullifier = ExternalNullifier::with_date_marker(date_marker, pbh_nonce);
+        let external_nullifier_hash = EncodedExternalNullifier::from(external_nullifier).0;
+
+        let Some(from) = self.0.from else {
+            todo!("TODO: handle error");
+        };
+
+        let signal_hash = hash_to_field(&SolValue::abi_encode_packed(&(from, calls.clone())));
+
+        let root = proof.root;
+        let semaphore_proof = semaphore_rs::protocol::generate_proof(
+            &identity,
+            &proof.proof,
+            external_nullifier_hash,
+            signal_hash,
+        )?;
+
+        let nullifier_hash =
+            semaphore_rs::protocol::generate_nullifier_hash(&identity, external_nullifier_hash);
+
+        //TODO: add helper functions to create the PBH payload
+        let payload = PBHPayload {
+            root,
+            nullifier_hash,
+            external_nullifier,
+            proof: PBHProof(semaphore_proof),
+        };
+
+        let calldata = IPBHEntryPoint::pbhMulticallCall {
+            calls,
+            payload: payload.into(),
+        };
+
+        Ok(Self(self.0.input(TransactionInput::new(
+            calldata.abi_encode().into(),
+        ))))
+    }
+
+    pub async fn build(self, signer: PrivateKeySigner) -> Result<TxEnvelope> {
+        Ok(self.0.build::<EthereumWallet>(&signer.into()).await?)
+    }
+}
+
+/// Generates calldata for the PBH King of the Hill game, where the provided player
+/// earns a point if they successfully capture the flag.
+pub fn king_of_the_hill_calldata(player: Address) -> Bytes {
+    crate::bindings::IPBHKotH::ctfCall { receiver: player }
+        .abi_encode()
+        .into()
+}
+
+/// Generates a multicall call, targeting the PBH King of the hill contract.
+/// The provided player will earn a point if they successfully capture the flag.
+pub fn king_of_the_hill_multicall(player: Address) -> Vec<Call3> {
+    let call = IMulticall3::Call3 {
+        target: PBH_CTF_CONTRACT,
+        callData: crate::bindings::IPBHKotH::ctfCall { receiver: player }
+            .abi_encode()
+            .into(),
+        allowFailure: false,
+    };
+
+    vec![call]
 }
 
 /// Builds and signs a CTF transaction.
