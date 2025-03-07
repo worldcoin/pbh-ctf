05515697e7870ddf8ba7c03bc4c10d442b1cdb83
0xOsiris <djosiris@proton.me>
2025-03-06 18:26:10 -0700

    chore: fmt

--- script/DeployRewardDistributor.s.sol
+++ script/DeployRewardDistributor.s.sol
@@ -6,7 +6,7 @@ import {RewardDistributor} from "src/RewardDistributor.sol";
 
 contract Deploy is Script {
     RewardDistributor public rewardDistributor;
-    address public constant CLAIMANT = 0xEd9d6ab46080aF6c75EF96B7293a4118F94Bc4e0; 
+    address public constant CLAIMANT = 0xEd9d6ab46080aF6c75EF96B7293a4118F94Bc4e0;
     address public constant USDC = 0x79A02482A880bCE3F13e09Da970dC34db4CD24d1;
     uint256 public constant REWARD = 3000e6;
 
